ded6890eb72a33dfee8be35733d217892c521113
Yupi7 <ekici.eyup@hotmail.de>
2026-06-15 22:13:41 +0200

    Added job details page

--- src/main/java/de/eyuepekici/ats/controller/JobController.java
+++ src/main/java/de/eyuepekici/ats/controller/JobController.java
@@ -1,9 +1,11 @@
 package de.eyuepekici.ats.controller;
 
+import de.eyuepekici.ats.entity.Job;
 import de.eyuepekici.ats.repository.JobRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class JobController {
@@ -16,12 +18,17 @@ public class JobController {
 
     @GetMapping("/jobs")
     public String jobs(Model model) {
-
-        model.addAttribute(
-                "jobs",
-                jobRepository.findAll()
-        );
-
+        model.addAttribute("jobs", jobRepository.findAll());
         return "jobs";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public String jobDetails(@PathVariable Long id, Model model) {
+        Job job = jobRepository.findById(id)
+                .orElseThrow();
+
+        model.addAttribute("job", job);
+
+        return "job-details";
     }
 }
